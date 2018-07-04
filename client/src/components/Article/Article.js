bc7cdcd3b483f603d571766014177b86367ae233
unknown <leabney@gmail.com>
2018-07-04 17:32:33 -0500

    formatting changes

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -4,22 +4,14 @@ import { ListItem } from "../List";
 
 const Article = ({ title, url, _id, date, handleClick, buttonText, saved }) => (
   <ListItem>
-    <h3>
-      <em>{title}</em>{" "}
-      <span className="btn-group pull-right">
-        <a
-          className="btn btn-light"
-          href={url}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          View Article
-        </a>
+    <h5>
+      <a href={url} target="_blank">{title}</a>
+      
         <button onClick={() => handleClick(_id)} className="btn btn-primary">
           {buttonText}
         </button>
-      </span>
-    </h3>
+     
+    </h5>
     <p>
       Date {saved ? "Saved" : "Published"}: {formatDate(date)}
     </p>
