276fc833c56db4e6d730463e9acd6b71915803f3
unknown <leabney@gmail.com>
2018-07-04 17:40:06 -0500

    formatting changes

--- client/src/components/Article/Article.js
+++ client/src/components/Article/Article.js
@@ -1,20 +1,21 @@
 import React from "react";
 import formatDate from "../../utils/formatDate";
 import { ListItem } from "../List";
+import "./Article.css";
 
 const Article = ({ title, url, _id, date, handleClick, buttonText, saved }) => (
   <ListItem>
     <h5>
       <a href={url} target="_blank">{title}</a>
       
-        <button onClick={() => handleClick(_id)} className="btn btn-primary">
+        <button onClick={() => handleClick(_id)} className="btn btn-info">
           {buttonText}
         </button>
      
     </h5>
-    <p>
+    <p><i>
       Date {saved ? "Saved" : "Published"}: {formatDate(date)}
-    </p>
+    </i></p>
   </ListItem>
 );
 
